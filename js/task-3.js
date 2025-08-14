9652b5e42d79d4a8da74d5259c93c71fc4569a68
vitvitcher <79949751+vitvitcher@users.noreply.github.com>
2025-08-14 10:57:50 +0300

    first try babyyyyy

---
+++ js/task-3.js
@@ -0,0 +1,8 @@
+'use strict';
+function getElementWidth(content, padding, border) {
+    return parseInt(content) + parseInt(padding) * 2 + parseInt(border) * 2
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
